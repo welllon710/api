5c6df05bc4ad154e199142ce8f0ac1991b60a11f
Your Name <you@example.com>
2020-12-04 15:40:07 +0800

    修复bug

--- app/controller/Wx.php
+++ app/controller/Wx.php
@@ -18,7 +18,7 @@ class Wx extends BaseController
         if ($argc->isEmpty()){
             $this->return_msg([],'空空如也呢!',400);
         }else{
-            $this->return_msg($openid,'请求成功',200);
+            $this->return_msg($argc,'请求成功',200);
         }
     }
 
@@ -49,7 +49,7 @@ class Wx extends BaseController
     {
          $openid = input('post.code');
          $read = \app\model\Wx::where('openid',$openid)->find();
-         $user = $read->article->visible(['id','title','tags','read_time'])->toArray();
+         $user = $read->article->visible(['id','title','tags','read_time']);
         if (empty($user)){
             $this->return_msg([],'您还没有阅读呢,赶快去阅读吧',400);
         }else{
